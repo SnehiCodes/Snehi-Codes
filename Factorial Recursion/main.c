065856d856d7fc8386fbed643bd6f10dfa528263
Snehi <111358189+SnehiCodes@users.noreply.github.com>
2022-09-08 22:14:49 +0530

    Add files via upload

---
+++ Factorial Recursion/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+int main() {
+    char word[20];
+    int n,left,right,i,t,len;
+
+    printf("Enter a number.");
+    scanf("%s",word);
+
+
+    len=strlen(word);
+    t=len;
+    left=0;
+    right=t-1;
+    //for(i=right;i>=left;i--){
+       // printf("%c",i);
+    //}
+    printf("%c",right);
+
+
+
+
+}
