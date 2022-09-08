95fd9f2d51d8a5bc0383fe58e737e3eccd0cc0de
Snehi <111358189+SnehiCodes@users.noreply.github.com>
2022-09-08 22:19:29 +0530

    Add files via upload

---
+++ fibonacci/Untitled1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+main(){
+    long int d,m,y;
+long int arr[7]={5,6,7,1,9,5,2,1};
+
+printf("month is %ld%ld \n",arr[2],arr[3]);
+printf("year is %ld%ld%ld%ld",arr[4],arr[5],arr[6],arr[7]);
+
+
+
+}
