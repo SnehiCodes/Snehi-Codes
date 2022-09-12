7b1ebc1728ee7386183eedbb25c6161691eb00a2
Snehi <111358189+SnehiCodes@users.noreply.github.com>
2022-09-12 19:57:34 +0530

    Add files via upload

---
+++ Recursion/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main() {
+    int n, char i, char word[20],left,right;
+    printf("Enter a number.");
+    scanf("%s",word);
+
+    strlen(word)='t';
+    left=0;
+    right=t-1;
+    for(i=right;i<=left;i--){
+        printf("%c",i);
+    }
+
+
+
+
+}
