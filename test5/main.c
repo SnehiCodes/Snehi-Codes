bd17e52d476435f83ec59971b28378d878f55292
Snehi <111358189+SnehiCodes@users.noreply.github.com>
2022-09-15 18:19:20 +0530

    Add files via upload

---
+++ test5/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+void main()
+{
+    double ch;
+    printf("enter a value between 1 to 2:");
+    scanf("%lf", &ch);
+    switch(ch)
+    {
+        case1:
+            printf("1");
+            break;
+        case2:
+            printf("1");
+            break;
+
+
+    }
+}
