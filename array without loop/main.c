eded9c45cc2960fbb2e6b7bada95e34ad53e15c8
Snehi <111358189+SnehiCodes@users.noreply.github.com>
2022-09-22 22:23:11 +0530

    Add files via upload

---
+++ array without loop/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include<string.h>
+
+
+
+
+
+int main()
+{  int[] arr = { 1, 2, 3, 4, 5, 6 };
+
+printf("The concatenated string is : %s",s1);
+printf(strcat(",", arr));
+    return 0;
+}
+}
