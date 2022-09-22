ad4935675b6618c9d1ecea0c23def7b0709965cc
Snehi <111358189+SnehiCodes@users.noreply.github.com>
2022-09-22 22:25:04 +0530

    Add files via upload

---
+++ const/main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+int main()
+{
+    int i, j, k,a;
+    a=4
+    ;
+    for(i=1;i<a;i++)
+    {
+        for(j=i;j<a;j++)
+        {
+            printf(" ");
+        }
+        for(k=1;k<(i*2);k++)
+        {
+                printf("*");
+        }
+        printf("\n");
+    }
+        for(i=a;j<a;k++)
+        {
+                printf(" ");
+        for(i=a;j<a;k++)
+        {
+                printf(" ");
+
+        }
+        for(i=a;j=((a/2)+1);k++)
+        {
+                printf("*");
+        printf("\n");
+   }
+    return 0;
+}}
+
+
+
